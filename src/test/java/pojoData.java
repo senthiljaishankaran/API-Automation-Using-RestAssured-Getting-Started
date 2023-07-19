bfe8ddaa4548b667bafda09449e245c24c38d957
SENTHIL KUMAR <senthiljaishankaran@gmail.com>
2023-07-19 15:22:48 +0530

    Created a POJO class for Payload

---
+++ src/test/java/pojoData.java
@@ -0,0 +1,23 @@
+public class pojoData {
+String name;
+String role;
+String lang[];
+public void setName(String name){
+    this.name=name;
+}
+public String getName(){
+    return this.name;
+}
+public void setRole(String role){
+    this.role=role;
+}
+public String getRole(){
+    return this.role;
+}
+public void setLang(String[] lang){
+    this.lang=lang;
+}
+public String[] getLang(){
+    return this.lang;
+}
+}
